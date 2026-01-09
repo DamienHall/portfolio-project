4809b6a7401e481f7acae00f057f26fda13f0804
damien-hall <damien.hall@hp.com>
2026-01-09 13:40:45 -0600

    Created boilerplate for starting the project
    Created the boilerplate ReactTS project, including tanstack code routing
    and ESLint toolchain

---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from '@tanstack/react-router'
+import Header from '@/components/Header'
+
+export const Route = createFileRoute('/')({
+  component: () => <div>
+		<Header />
+	</div>,
+})
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [devtools(), viteReact(), tailwindcss(), tanstackRouter({
+		autoCodeSplitting: true,
+	})],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
