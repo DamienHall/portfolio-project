e7405b752456ae52f90a38c775988a26b732aa97
DamienHall <damien.z.hall@gmail.com>
2026-03-14 01:30:42 -0500

    Updated the UI to use a ContentBackground with all the project cards in it instead.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,25 +2,30 @@ export default function ProjectCard({
   imageURL,
   title,
   description,
+	ghLink,
 	tags=[],
 }: {
   imageURL: string
   title: string
   description: string
+	ghLink: string,
 	tags: Array<string>
 }) {
   return (
-    <div className="particleInteractive">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg backdrop-blur-sm bg-white/30">
+    <div className="pt-8 w-4/5">
+      <div className="particleInteractive rounded overflow-hidden shadow-lg bg-coldfoam">
         <img className="" src={imageURL} alt="Project Image" />
         <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{title}</div>
-          <p className="text-gray-700 text-base">{description}</p>
+          <div className="font-bold text-xl mb-2 text-mocha border-b-2 p-6 w-2/5">{title}</div>
+					<p className="text-mocha text-base font-semibold">
+					Checkout the project here:
+					<span className="text-almond"><a href={ghLink}> {ghLink}</a></span></p>
+          <p className="pt-4 text-mocha text-base whitespace-pre-wrap">{description}</p>
         </div>
         <div className="px-6 pt-4 pb-2">
 					{
 						tags.map((tag, index) => {
-							return <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+							return <span key={index} className="inline-block bg-almond rounded-full px-3 py-1 text-sm font-semibold text-coldfoam mr-2 mb-2">
 								{tag}
 							</span>
 						})
--- src/routes/module-projects.tsx
+++ src/routes/module-projects.tsx
@@ -1,35 +1,51 @@
 import { createFileRoute } from '@tanstack/react-router'
 import InteractiveBackground from '@/components/InteractiveBackground'
 import ProjectCard from '@/components/ProjectCard'
-import Carousel from '@/components/Carousel/Carousel'
+import ContentBackground from '@/components/ContentBackground'
 
 export const Route = createFileRoute('/module-projects')({
   component: () => (
-    <div id="root">
+    <div id="root" className="flex justify-center">
       <InteractiveBackground />
-      <h1 className="text-3xl font-bold text-white p-8 w-fit backdrop-blur-sm rounded">
-        Module Projects
-      </h1>
-      <Carousel>
+			<ContentBackground>
         <ProjectCard
           imageURL="../../public/project1.jpg"
-          title="DM-DriveAnalyzer Project"
-          description="A drive migration tool built in .Net/C#."
-          tags={['C#', '.Net', 'Linux', 'Raspberry Pi']}
-        />
-        <ProjectCard
-          imageURL="../../public/project2.png"
-          title="DM-DriveAnalyzer Project"
-          description="A drive migration tool built in .Net/C#."
-          tags={['C#', '.Net', 'Linux', 'Raspberry Pi']}
+          title="Front End Project"
+          description={`> This project was created to show my learned skills in Backend Development, specifically regarding <tagsstart>SQLite, MySQL, Golang, API's, Google UUID, gorm<tagsend>. This project was inspired by the various open-source self-hosted media server projects out there on the internet. After learning more and more about how easy it is to track people through their data and how companies like Google use our data to train AI we came up with the thought to try our hand at making our own self-hosted media server with tagging capabilities.\n> This project includes a SQL database to store not only photos but videos as well. Alongside the video storage and retrieval methods we have also added the ablitiy to interact with that data by tagging it. Tags can be added, edited, and attached to different type of media and be used to filter results when calling to the database. What good is all of this functionality if only one person can use it? The answer is not much, that's why we also used Google UUID and JWT's to allow for specific users to see their specific data in the database.\n> I contributed mostly in the creation of the tagging system, both on the API route side and the database side. I created a database that is tag specific and related it to the media data using GORM. I also helped with the authentication side of everything mostly in regards to implementing the JWT and middleware handling for the authorized routes.`}
+					ghLink="https://github.com/DamienAndDustinBackend/backend-project"
+          tags={["SQLite", "MySQL", "Golang", "API's"]}
         />
         <ProjectCard
           imageURL="../../public/project1.jpg"
-          title="DM-DriveAnalyzer Project"
-          description="A drive migration tool built in .Net/C#."
-          tags={['C#', '.Net', 'Linux', 'Raspberry Pi']}
+          title="Back End Project"
+          description=""
+          tags={[]}
         />
-      </Carousel>
+        <ProjectCard
+          imageURL="../../public/project1.jpg"
+          title="Deployment Project"
+          description=""
+          tags={[]}
+        />
+        <ProjectCard
+          imageURL="../../public/project1.jpg"
+          title="Final Bootcamp Project"
+          description=""
+          tags={[]}
+        />
+        <ProjectCard
+          imageURL="../../public/project1.jpg"
+          title="Inventory Full Stack Project"
+          description=""
+          tags={[]}
+        />
+        <ProjectCard
+          imageURL="../../public/project1.jpg"
+          title="Backend Project"
+          description=""
+          tags={[]}
+        />
+			</ContentBackground>
     </div>
   ),
 })
